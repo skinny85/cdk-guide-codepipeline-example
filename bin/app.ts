fa94afec0694cebe304e30130dd3171adffb5003
Adam Ruka <adamruka@amazon.com>
2020-12-18 09:46:02 -0800

    Standard changes.

--- bin/app.ts
+++ bin/app.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkGuideCodepipelineExampleStack } from '../lib/cdk-guide-codepipeline-example-stack';
